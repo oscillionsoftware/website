84513130e31c98a2c361fedcdadf220b5c27a24c
ram <ramswami0021@gmail.com>
2025-11-08 15:34:48 +0530

    hi

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -282,11 +282,11 @@ const Navigation = () => {
         <div className="flex items-center justify-between h-20">
           <div className="flex-shrink-0">
             <Link to="/" className="flex items-center">
-              <img
-                src="/Oscillion Software-02.png"
-                alt="Oscillion Software"
-                className="h-12 w-auto"
-              />
+             <img
+  src="/Oscillion Software-02.png"
+  alt="Oscillion Software"
+  className="h-12 w-auto logo-mobile"
+/>
             </Link>
           </div>
 
